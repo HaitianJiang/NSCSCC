bc6f301677fde59560d5c4518c8857e52fd540fc
HaitianJiang <62015641+HaitianJiang@users.noreply.github.com>
2020-11-27 15:54:20 +0800

    ALU design and test finish
    完成了ALU的设计和测试

---
+++ PersonalProgress/HaitianJiang/SingleMiniSys/mycpu_design/mycpu_design.srcs/sim_1/new/tb_datapath_1.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/11/27 12:12:14
+// Design Name: 
+// Module Name: tb_datapath_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_datapath_1;
+
+// datapath_1 Parameters
+parameter PERIOD  = 10;
+
+
+// datapath_1 Inputs
+reg   clk                                  = 0 ;
+reg   rst_n                                = 0 ;
+
+// datapath_1 Outputs
+wire  [31:0]  instruction                  ;
+
+
+initial
+begin
+    forever #(PERIOD/2)  clk=~clk;
+end
+
+initial
+begin
+    #(PERIOD*2) rst_n  =  1;
+end
+
+datapath_1  u_datapath_1 (
+    .clk                     ( clk                 ),
+    .rst_n                   ( rst_n               ),
+
+    .instruction             ( instruction  [31:0] )
+);
+
+
+endmodule
---
+++ PersonalProgress/HaitianJiang/SingleMiniSys/mycpu_design/mycpu_design.srcs/sources_1/new/datapath_1.v
@@ -0,0 +1,125 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 2020/11/27 11:41:34
+// Design Name:
+// Module Name: datapath_1
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description: 仅仅实现了几个简单的R类指令的最简单的数据通路，不与外界交互
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module datapath_1(
+           input clk,
+           input rst_n,
+
+           output [31:0] instruction
+       );
+
+/******** PC ********/
+
+// pc_1 Outputs
+wire  [31:0]  pcOld;
+
+pc_1  u_pc_1 (
+          .clk                     ( clk     ),
+          .rst_n                   ( rst_n   ),
+          .pcNew                   ( pcOld   ), // pcNew = pcOld + 4; no selection
+
+          .pcOld                   ( pcOld   )
+      );
+
+
+/******** Instruction ROM ********/
+
+// blk_mem_gen_0 Inputs
+wire  [13:0]  addra  = pcOld[15:2];
+
+// blk_mem_gen_0 Outputs // instructions
+// wire  [31:0]  instruction;
+
+blk_mem_gen_0  u_blk_mem_gen_0 (
+                   .clka                    ( clk    ),
+                   .addra                   ( addra   ),
+
+                   .douta                   ( instruction   )
+               );
+
+
+/******** Reg Files ********/
+//
+//  // reg_files_1 Inputs
+//  wire   [4:0]  rA = instruction[25:21];
+//  wire   [4:0]  rB = instruction[20:16];
+//  wire   [4:0]  rW = instruction[15:11];
+//  wire   [31:0]  writeData;
+//  wire   RegWrite;
+//
+//  // reg_files_1 Outputs
+//  wire  [31:0]  A;
+//  wire  [31:0]  B;
+//
+//  reg_files_1  u_reg_files_1 (
+//      .clk                     ( clk         ),
+//      .rst_n                   ( rst_n       ),
+//      .rA                      ( rA          ),
+//      .rB                      ( rB          ),
+//      .rW                      ( rW          ),
+//      .writeData               ( writeData   ),
+//      .RegWrite                ( RegWrite    ),
+//
+//      .A                       ( A           ),
+//      .B                       ( B           )
+//  );
+//
+//
+//  /******** ALU ********/
+//
+//  // ALU_1 Inputs
+//  // wire   [31:0]  A;
+//  // wire   [31:0]  B;
+//  wire   [3:0]  ALUop;
+//
+//  // ALU_1 Outputs
+//  wire  [31:0]  ALUresult = writeData;
+//
+//  ALU_1  u_ALU_1 (
+//      .A                       ( A           ),
+//      .B                       ( B           ),
+//      .ALUop                   ( ALUop       ),
+//
+//      .ALUresult               ( ALUresult   )
+//  );
+//
+//
+//  /******** controler ********/
+//
+//  // control_1 Inputs
+//  wire   [5:0]  op = instruction[31:26];
+//  wire   [5:0]  func = instruction[5:0];
+//
+//  // control_1 Outputs
+//  // wire  RegWrite
+//  // wire  [3:0]  ALUop;
+//
+//  control_1  u_control_1 (
+//      .op                      ( op         ),
+//      .func                    ( func       ),
+//
+//      .RegWrite                ( RegWrite   ),
+//      .ALUop                   ( ALUop      )
+//  );
+//
+endmodule
+
